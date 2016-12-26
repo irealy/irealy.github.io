d3a54412172be0e61c0f91d30d2bb64f1784bea4
irealy <b.b.kononov@gmail.com>
2016-12-26 23:10:52 +0200

    update

--- demo/test/src/js/app.js
+++ demo/test/src/js/app.js
@@ -1,3 +1,11 @@
+// example of simple includes for js
+//=include lib/sayHello.js
+//=include lib/jquery.min.js
+//=include lib/slick.min.js
+//=include lib/svgxuse.min.js
+
+// sayHello();
+
 $(document).ready(function() {
 	$('.burger-menu').on("click", function(e) {
 		e.preventDefault();
