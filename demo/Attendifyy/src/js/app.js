3826a92c9be200adb5fbbc8d012fd12825ce9cd7
irealy <b.b.konononv@gmail.com>
2017-01-18 18:34:15 +0200

    update attendify

--- demo/Attendifyy/src/js/app.js
+++ demo/Attendifyy/src/js/app.js
@@ -5,3 +5,34 @@
 //=include lib/svgxuse.min.js
 
 sayHello();
+
+$(document).ready(function() {
+
+	tabs();
+
+});
+
+function tabs() {
+
+	$('.app-tabs__content:not(:first)').hide();
+
+	$('.app-tabs__item').each(function(i) {
+		var self = $(this);
+		self.attr('data-tab', 'tab-' + i);
+	});
+
+	$('.app-tabs__content').each(function(i) {
+		var me = $(this);
+		me.attr('data-tab', 'tab-' + i);
+	});
+
+	$('.app-tabs__item').click(function() {
+		var self = $(this);
+		var dataTab = self.data('tab');
+		self.addClass('app-tabs__item_active').siblings().removeClass('app-tabs__item_active');
+		$('.app-tabs__content').hide();
+		$('.app-tabs__content[data-tab=' + dataTab + ']').fadeIn('normal');
+
+	});
+
+};
